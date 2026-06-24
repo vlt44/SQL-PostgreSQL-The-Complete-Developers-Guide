8ec7f797ed23dd29de5f011add46526263dd8a9c
vantaylo <vanessalyn.taylor@gmail.com>
2026-06-24 16:19:53 -0500

    three way join

--- Project2/photosDB_comments.sql
+++ Project2/photosDB_comments.sql
@@ -178,3 +178,9 @@ VALUES
   FROM comments
   JOIN photos ON photos.id = comments.photo_id
   WHERE comments.user_id = photos.user_id;
+
+-- all photos and comments where the author of the photo and comment is this user
+  SELECT url, contents, username
+  FROM comments
+  JOIN photos ON photos.id = comments.photo_id
+  JOIN users ON users.id = comments.user_id AND users.id = photos.user_id;
