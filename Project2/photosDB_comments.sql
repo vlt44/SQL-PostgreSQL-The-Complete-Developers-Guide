8d2797af850f0d1a6ce9fcd292a1cc63692f446c
vantaylo <vanessalyn.taylor@gmail.com>
2026-06-24 16:55:16 -0500

    Aggragate functions

--- Project2/photosDB_comments.sql
+++ Project2/photosDB_comments.sql
@@ -190,3 +190,9 @@ VALUES
   FROM comments
   GROUP BY user_id;
 
+-- Aggragate functions
+  SELECT MIN(id) FROM comments;
+  SELECT MAX(id) FROM comments;
+  SELECT COUNT(id) FROM comments;
+  SELECT AVG(id) FROM comments; 
+  SELECT SUM(id) FROM comments;
