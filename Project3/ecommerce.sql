d39ded0991c1623454597434887db15c6215bb1a
vantaylo <vanessalyn.taylor@gmail.com>
2026-07-08 16:58:00 -0500

    correlated subquery in where clause

--- Project3/ecommerce.sql
+++ Project3/ecommerce.sql
@@ -961,3 +961,12 @@ WHERE price >  SOME (
   FROM products
   WHERE department = 'Industrial'
 );
+
+-- Show name, department and price of the most expensive product in each department
+SELECT name, department, price
+FROM products AS p1
+WHERE price = (
+  SELECT MAX(price)
+  FROM products AS p2
+  WHERE p1.department = p2.department
+);
