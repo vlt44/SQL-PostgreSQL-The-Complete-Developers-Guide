66826d0da5cb221e381441f8dbe5e4c592c46b89
vantaylo <vanessalyn.taylor@gmail.com>
2026-09-19 15:45:40 -0500

    benchmarking indexes

--- Project6/internals.sql
+++ Project6/internals.sql
@@ -16,3 +16,8 @@ CREATE INDEX ON users (username);
 
 -- Delete an index on the username column
 DROP INDEX users_username_idx;
+
+-- Analyze the query plan. With index it took: 0.075ms compares to 1.249ms without index.
+EXPLAIN ANALYZE SELECT *
+FROM users
+WHERE username = 'Emil30';
